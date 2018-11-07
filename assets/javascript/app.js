96f85774d691a0f6ededf7500743004d8f9ff017
Bayli Sade <sade.bayli1@gmail.com>
2018-11-07 01:07:28 -0500

    fixed bug and now I am able to see my trains pop up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,6 +36,33 @@ var config = {
       $("#destination-input").val("");
       $("#first-train-input").val("");
       $("#frequency-input").val("");
+
+      var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years"); // MomentJS
+    console.log(firstTimeConverted);
+
+    // Current time using MomentJS
+    var currentTime = moment(); // MomentJS
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm")); // MomentJS
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes"); // MomentJS
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequencyInput;
+    console.log(tRemainder);
+
+    // Minute until train arrives
+    var tMinutesTillTrain = frequencyInput - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes"); // MomentJS
+    var nextTrainConverted = moment(nextTrain).format("hh:mm a"); // MomentJS
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm")); // MomentJS
+
+    // Add each trains data into the HTML table
+    $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + "Every " + frequencyInput + " minutes" + "</td><td>" + nextTrainConverted + "</td><td>" + tMinutesTillTrain + "</td></tr>");
   })
 
     database.ref().on("child_added", function(childSnapshot){
@@ -50,32 +77,4 @@ var config = {
         console.log(trainDestination);
         console.log(firstTrain);
         console.log(trainFreq);
-
-        // Convert first time (push back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years"); // MomentJS
-    console.log(firstTimeConverted);
-
-    // Current time using MomentJS
-    var currentTime = moment(); // MomentJS
-    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm")); // MomentJS
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes"); // MomentJS
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequency;
-    console.log(tRemainder);
-
-    // Minute until train arrives
-    var tMinutesTillTrain = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes"); // MomentJS
-    var nextTrainConverted = moment(nextTrain).format("hh:mm a"); // MomentJS
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm")); // MomentJS
-
-    // Add each trains data into the HTML table
-    $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + "Every " + frequency + " minutes" + "</td><td>" + nextTrainConverted + "</td><td>" + tMinutesTillTrain + "</td></tr>");
 })
